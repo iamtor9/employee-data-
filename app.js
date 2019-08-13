d8afa933e4cebee8df3f6b0fa3bedcb1b5f23eaa
Jason Ostergren <jason@stp-er-10-129-85-204.wireless.umn.edu>
2019-08-13 13:06:25 -0500

    javascript stuff

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-function getArray(cb) {
-    localforage.getItem("array").then(function (result) {
-    cb(result || []); //{name, score}
-    });
-    }
+// function getArray(cb) {
+//     localforage.getItem("array").then(function (result) {
+//     cb(result || []); //{name, score}
+//     });
+//     }
 
-    function setArray (newArray, cb) {
-        localforage.setItem("array", newArray).then(cb);
-       }
-       
+//     function setArray (newArray, cb) {
+//         localforage.setItem("array", newArray).then(cb);
+//        }
+      
+document.querySelector("#submitButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    submitInfo();
+})
+
+function submitInfo() {
+console.log("you clicked")
+document.getElementById("formGroupExampleInput").value
+document.getElementById("formGroupExampleInput2").value
+document.getElementById("formGroupExampleInput3").value
+document.getElementById("formGroupExampleInput4").value
+
+};
+

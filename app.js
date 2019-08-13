13121b60b42f814b991da3c21f77e5a2e533c58e
Nolan Hewitt <nolanhewitt@aol.com>
2019-08-13 14:03:04 -0500

    localforage works

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// function getArray(cb) {
-//     localforage.getItem("array").then(function (result) {
-//     cb(result || []); //{name, score}
-//     });
-//     }
-
-//     function setArray (newArray, cb) {
-//         localforage.setItem("array", newArray).then(cb);
-//        }
       
 document.querySelector("#submitButton").addEventListener("click", function(event) {
     event.preventDefault();
@@ -19,6 +10,39 @@ const input1 = document.getElementById("formGroupExampleInput").value
 const input2 = document.getElementById("formGroupExampleInput2").value
 const input3 = document.getElementById("formGroupExampleInput3").value
 const input4 = document.getElementById("formGroupExampleInput4").value
-console.log(input1, input2, input3, input4);
+array.push(input1, input2, input3, input4);
 };
 
+var array = []
+console.log(array);
+//add click listener on submitbutton
+document.querySelector("#submitButton").addEventListener("click", function(){
+    //
+    const value = document.querySelector(".form-control").value;
+    localforage.getItem("saved").then(function(result){
+      if(!result){
+        result = [];
+      }
+      result.push(value);
+      localforage.setItem("saved", result)
+        .then(function(){
+          console.log("saved")
+          console.log(result);
+        });
+    });
+    
+  });
+  
+  const div = document.querySelector("div");
+  setInterval(function(){
+    localforage.getItem("saved").then(function(result){
+      if(!result){
+        result = [];
+      }
+      div.innerHTML = "";
+      for(let i = 0; i<result.length; i++){
+        const br = document.createElement("br");
+        div.append(br, result[i])
+      }
+    });
+  }, 5000000000)
